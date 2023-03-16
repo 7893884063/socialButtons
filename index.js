8639c0d775234675a8fc79e95c324623ee03221a
7893884063 <anilkumar1003200@gmail.com>
2023-03-16 14:10:31 +0000

    social Butoons

---
+++ index.js
@@ -0,0 +1,18 @@
+const Button = (props) => {
+  const { className, buttonText } = props;
+  return <button className={className}>{buttonText}</button>;
+};
+
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Social Buttons</h1>
+    <div>
+      <Button className="like-button" buttonText="Like" />
+      <Button className="comment-button" buttonText="comment" />
+      <Button className="share-button" buttonText="share" />
+    </div>
+  </div>
+  //  Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
